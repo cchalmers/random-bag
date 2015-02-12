9b23aa9b94872709dd02d655ae590e0ae9c2074a
Christopher Chalmers <c.chalmers@me.com>
2015-02-12 14:48:10 +0000

    Added more functions.

--- src/Data/RandomBag.hs
+++ src/Data/RandomBag.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE BangPatterns              #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
@@ -19,7 +20,7 @@
 --  All Operations are in a `PrimMonad` for efficiency.
 --
 
-module Data.Bag where
+module Data.RandomBag where
 
 import           Control.Monad
 import           Control.Monad.Primitive
@@ -27,8 +28,10 @@ import           Control.Monad.ST
 import           Data.Data
 import           Data.PRef
 import qualified Data.Vector.Binary          ()
+import           Data.Vector.Unboxed         (Vector, Unbox)
 import qualified Data.Vector.Unboxed         as V
 import qualified Data.Vector.Unboxed.Mutable as M
+import Control.DeepSeq
 
 import           System.Random.PCG.Class
 
@@ -41,7 +44,7 @@ data Bag m a where
   deriving Typeable
 
 -- | Contruct a bag from a list, using the given generator
-mkBag :: (PrimMonad m, M.Unbox a, Generator g m) => g -> [a] -> m (Bag m a)
+mkBag :: (PrimMonad m, Unbox a, Generator g m) => g -> [a] -> m (Bag m a)
 mkBag g as = do
   let v = V.fromList as
   thawBag g 0.2 v
@@ -49,40 +52,88 @@ mkBag g as = do
 type IOBag   = Bag IO
 type STBag s = Bag (ST s)
 
+size :: PrimMonad m => Bag m a -> m Int
+size (Bag _ r _) = readPRef r
+{-# INLINE size #-}
+
+resize :: PrimMonad m => Bag m a -> Int -> m ()
+resize (Bag _ r _) = \i -> writePRef r i
+{-# INLINE resize #-}
+
+-- internal ------------------------------------------------------------
+
+bagUniform :: PrimMonad m => Bag m a -> Int -> m Int
+bagUniform (Bag g _ _) i = uniformR (0, i - 1) g
+{-# INLINE bagUniform #-}
+
+unsafeRead :: (PrimMonad m, Unbox a) => Bag m a -> Int -> m a
+unsafeRead (Bag _ _ v) = \a -> M.unsafeRead v a
+{-# INLINE unsafeRead #-}
+
+randomIndex :: PrimMonad m => Bag m a -> m Int
+randomIndex bag = size bag >>= bagUniform bag
+{-# INLINE randomIndex #-}
+
+withRandom :: PrimMonad m
+           => Bag m a
+           -> (M.MVector (PrimState m) a -> Int -> Int -> m b)
+           -> m (Maybe b)
+withRandom (Bag g r mv) f = do
+  n <- readPRef r
+  if n == 0
+    then return Nothing
+    else do
+      i <- uniformR (0, n - 1) g
+      b <- f mv i n
+      return (Just $! b)
+
+vRemove :: (PrimMonad m, Unbox a)
+        => M.MVector (PrimState m) a
+        -> Int -- last element
+        -> Int -- element to remove
+        -> m ()
+vRemove mv i n = M.read mv (n - 1) >>= M.write mv i
+
+-- | Take a random element from the bag.
+luckyDip :: (PrimMonad m, Unbox a) => Bag m a -> m (Maybe a)
+luckyDip bag = liftM join . withRandom bag $ \mv i n -> do
+  a <- M.read mv i
+  vRemove mv i n
+  resize bag (n - 1)
+  return (Just $! a)
+{-# INLINE luckyDip #-}
+
+-- | Take a random element from the bag and
+luckyPeek :: (PrimMonad m, Unbox a)
+         => Bag m a -> m (Maybe a)
+luckyPeek bag = withRandom bag $
+  \mv i _ -> M.read mv i
+{-# INLINE luckyPeek #-}
+
 -- | Check if a bag is empty.
 empty :: PrimMonad m => Bag m a -> m Bool
-empty (Bag _ r _) = do
-  m <- readPRef r
-  return $ m == 0
+empty = liftM (==0) . size
+{-# INLINE empty #-}
 
--- | Select a random value from the bag. Returns an error if the bag is
---   empty.
-luckyDip :: (PrimMonad m, M.Unbox a) => Bag m a -> m a
-luckyDip (Bag g r v) = do
-  m <- readPRef r
-  when (m == 0) $ error "luckyDip: empty bag"
-  i <- uniformR (0, m - 1) g
-  M.read v i
+-- -- | Select a random value from the bag. Returns nothing
+-- --   empty.
+-- luckyDip :: (PrimMonad m, Unbox a) => Bag m a -> m (Maybe a)
+-- luckyDip (Bag g r v) = do
+--   n <- size
+--   if n == 0
+--     then Nothing
+--     else do
+--       i <- bagUniform bag n
+--       a <- unsafeRead bag i
+--       return (Just $! a)
+-- {-# INLINE luckyDip #-}
+
+-- | Select a random value from the bag.
+unsafeLuckyDip :: (PrimMonad m, Unbox a) => Bag m a -> m a
+unsafeLuckyDip bag = randomIndex bag >>= unsafeRead bag
+{-# INLINE unsafeLuckyDip #-}
 
 {-
--- | Take a random element from the bag so it's no longer in the bag.
-luckyDip :: (PrimMonad m, MVector v a) => Bag m v a -> m (Maybe a)
-
--- | Look at random element in the bag but don't take it out.
-luckyPeek :: (PrimMonad m, MVector v a) => Bag m v a -> m (Maybe a)
-
--- | Look at random element in the bag but don't take it out. Throws an
---   error if the bag is empty.
-unsafePeek :: (PrimMonad m, MVector v a) => Bag m v a -> m a
-
--- | O(n). Shuffle the bag, returning a vector of each element of the
---   bag in random order.
-shuffle :: Bag m v a -> m (v (PrimState m) a)
-
--- | O(n). Shuffle the bag, returning a vector of up to @n@ elements of
---   the bag in random order.
-shuffleN :: Int -> Bag m v a -> m (v (PrimState m) a)
-
 -- | O(n). Fold the elements of the bag in a random order.
 foldM :: (b -> a -> m b) -> b -> Bag m v a -> m b
 
@@ -92,12 +143,6 @@ take :: Int -> Bag m v a -> m (Bag m v a)
 
 -- | O(n). @add a b@ adds elements from bag @a@ to bag @b@.
 combine :: Bag m v a -> Bag m v a -> m ()
-
--- | O(n). Find an element in a bag.
-lookup :: Eq a => a -> Bag m v a -> m (Maybe a)
-
--- | O(n). Find an element in a bag.
-find :: (a -> Bool) -> Bag m v a -> m (Maybe a)
 
 filter :: (a -> Bool) -> Bag m v a -> m ()
 
@@ -117,52 +162,6 @@ data Modify a
   | Remove
   | Modify !a
 
-withElement :: (PrimMonad m, M.Unbox a) => Bag m a -> (a -> m b) -> m (Maybe b)
-withElement bag@(Bag _ r _) f = do
-  m <- readPRef r
-  if m == 0
-    then return Nothing
-    else do
-      a <- luckyDip bag
-      b <- f a
-      return (Just b)
-
--- | Modify some random element of a bag. Allows removal, keeping or
---   modifying. Does nothing if the bag is empty.
-rModify :: (PrimMonad m, M.Unbox a) => Bag m a -> (a -> Modify a) -> m ()
-rModify (Bag g r v) f = do
-  m <- readPRef r
-  if m == 0
-    then return ()
-    else do
-      i <- uniformR (0, m - 1) g
-      a <- M.read v i
-      case f a of
-        Keep     -> return ()
-        Modify b -> M.write v i b
-        Remove   -> do
-          l <- M.read v (m - 1)
-          M.write v i l
-          modifyPRef r (subtract 1)
-
--- | Modify some element of a bag. Allows removal, keeping or modifying.
---   Does nothing if the bag is empty.
-rModifyM :: (PrimMonad m, M.Unbox a) => Bag m a -> (a -> m (Modify a)) -> m ()
-rModifyM (Bag g r v) f = do
-  m <- readPRef r
-  if m == 0
-    then return ()
-    else do
-      i <- uniformR (0, m - 1) g
-      a <- M.read v i
-      f a >>= \case
-        Keep     -> return ()
-        Modify b -> M.write v i b
-        Remove   -> do
-          l <- M.read v (m - 1)
-          M.write v i l
-          modifyPRef r (subtract 1)
-
 -- | Helper to only modify when @b@ is 'True'.
 modifyWhen :: Monad m => Bool -> m (Modify a) -> m (Modify a)
 modifyWhen b a = if b then a else return Keep
@@ -171,14 +170,112 @@ modifyWhen b a = if b then a else return Keep
 modifyUnless :: Monad m => Bool -> m (Modify a) -> m (Modify a)
 modifyUnless b a = if b then return Keep else a
 
-elem :: (PrimMonad m, Eq a, M.Unbox a) => Bag m a -> a -> m Bool
-elem bag a = do
-  v <- unsafeFreezeBag bag
-  return $ a `V.elem` v
+modify :: (PrimMonad m, Unbox a) => Bag m a -> (a -> Modify a) -> m ()
+modify b f = modifyM b (return . f)
+{-# INLINE modify #-}
+
+modifyM :: (PrimMonad m, Unbox a) => Bag m a -> (a -> m (Modify a)) -> m ()
+modifyM bag f = liftM (const ()) . withRandom bag $
+  \mv i n -> do
+    a <- M.read mv i
+    f a >>= \case
+      Keep     -> return ()
+      Remove   -> vRemove mv i n >> resize bag (n - 1)
+      Modify b -> M.write mv i b
+{-# INLINE modifyM #-}
+
+alter :: (PrimMonad m, Unbox a) => Bag m a -> (a -> a) -> m ()
+alter bag f = modify bag (Modify . f)
+
+alterM :: (PrimMonad m, Unbox a) => Bag m a -> (a -> m a) -> m ()
+alterM bag f = modifyM bag (liftM Modify . f)
+
+-- vectors -------------------------------------------------------------
+
+-- | Use the frozen underlying vector. This is useful for making use of
+--   vector's many functions. The result has to be forced to ensure
+--   changes to the bag don't change the result.
+withVector :: (PrimMonad m, Unbox a, NFData b)
+           => (Vector a -> b) -> Bag m a -> m b
+withVector f bag = (force . f) `liftM` unsafeFreezeBag bag
+
+-- | Use the frozen underlying vector. This is useful for making use of
+--   vector's many functions. The result has to be forced to ensure
+--   changes to the bag don't change the result.
+withVectorM :: (PrimMonad m, Unbox a, NFData b)
+            => (Vector a -> m b) -> Bag m a -> m b
+withVectorM f bag = unsafeFreezeBag bag >>= liftM force . f
+
+-- | /O(n)/ Check if the bag contains an element.
+inBag :: (PrimMonad m, Unbox a, Eq a)
+     => a -> Bag m a -> m Bool
+inBag a = withVector (V.elem a)
+
+-- | /O(n)/ Check if the bag does not contain an element (inverse of 'elem').
+notInBag :: (PrimMonad m, Unbox a, Eq a)
+     => a -> Bag m a -> m Bool
+notInBag a = withVector (V.notElem a)
+
+-- | /O(n)/ Yield 'Just' the first element matching the predicate or
+--   'Nothing' if no such element exists.
+find :: (PrimMonad m, Unbox a, NFData a)
+     => (a -> Bool) -> Bag m a -> m (Maybe a)
+find f = withVector (V.find f)
+-- this could be implimented without the need for @deepseq a@
+
+-- | /worst case O(n)/ Find the first element that matches the predicate
+--   and modify it.
+findModify :: (PrimMonad m, Unbox a)
+           => Bag m a -> (a -> Bool) -> (a -> Modify a) -> m ()
+findModify bag@(Bag _ r mv) p f = do
+  n  <- readPRef r
+  mi <- V.findIndex p `liftM` unsafeFreezeBag bag
+  case mi of
+    Just i  -> M.unsafeRead mv i >>= \a ->
+      case f a of
+        Keep     -> return ()
+        Remove   -> vRemove mv i n >> resize bag (n - 1)
+        Modify b -> M.write mv i b
+    Nothing -> return ()
+
+-- | /O(n)/ Find the all elements that matche the predicate
+--   and modify them.
+-- findModifyAll :: (PrimMonad m, Unbox a)
+--            => Bag m a -> (a -> Bool) -> (a -> Modify a) -> m ()
+-- findModifyAll bag@(Bag _ r mv) p f = do
+--   n  <- readPRef r
+--   is <- V.findIndicies p `liftM` unsafeFreezeBag bag
+--   forM_ is $ \i -> M.unsafeRead mv i >>= \a ->
+--       case f a of
+--         Keep     -> return ()
+--         Remove   -> vRemove mv i n >> resize bag (n - 1)
+--         Modify b -> M.write mv i b
+
+-- random shuffle ------------------------------------------------------
+
+-- | /O(n)/ Perform a random shuffle on the bag, emptying it. Takes upto
+--   @m@ random elements.
+unsafeRandomShuffle :: (PrimMonad m, Unbox a) => Bag m a -> m (Vector a)
+unsafeRandomShuffle = unsafeRandomShuffleN maxBound
+{-# INLINE unsafeRandomShuffle #-}
+
+-- | /O(max n m)/ Perform a random shuffle on the bag. Takes upto
+--   @m@ random elements. The underlying vector of the bag is also
+--   randomised.
+unsafeRandomShuffleN :: (PrimMonad m, Unbox a) => Int -> Bag m a -> m (Vector a)
+unsafeRandomShuffleN m (Bag g r mv) = do
+  n <- readPRef r
+  let n' = max n m
+  for 0 n' $ \nn -> do
+    i <- uniformR (n, nn) g
+    M.swap mv i n
+
+  V.unsafeFreeze $ M.take (n' + 1) mv
+{-# INLINE unsafeRandomShuffleN #-}
 
 -- | O(n) Replace one element with another. Returns an error if the
 --   element isn't present.
-replace :: (PrimMonad m, Eq a, M.Unbox a)
+replace :: (PrimMonad m, Eq a, Unbox a)
         => Bag m a -> a -> a -> m ()
 replace bag@(Bag _ _ mv) a b = do
   v <- unsafeFreezeBag bag
@@ -193,34 +290,37 @@ replace bag@(Bag _ _ mv) a b = do
 --   Be aware the currently the underlying vector has no way to grow, so
 --   if you add too many elements, an error is throw. This should be
 --   fixed soon.
-insert :: (PrimMonad m, M.Unbox a) => Bag m a -> a -> m ()
+insert :: (PrimMonad m, Unbox a) => Bag m a -> a -> m ()
 insert (Bag _ r v) a = do
-  m <- readPRef r
-  when (m > M.length v) $ error "insert: Bag not large enough!"
+  n <- readPRef r
+  when (n > M.length v) $ error "insert: Bag not large enough!"
   -- TODO figure out a nice way to grow the vector
 
-  M.write v m a
+  M.write v n a
   plusOne r
 
 -- immutable -----------------------------------------------------------
 
 -- | Freeze a bag to retreive it's internal vector.
-freezeBag :: (PrimMonad m, M.Unbox a) => Bag m a -> m (V.Vector a)
+freezeBag :: (PrimMonad m, Unbox a) => Bag m a -> m (V.Vector a)
 freezeBag (Bag _ r v) = do
-  m <- readPRef r
-  let v' = M.take m v
+  n <- readPRef r
+  let v' = M.take n v
   V.freeze v'
 
 -- | Freeze a bag to retreive it's internal vector.
-unsafeFreezeBag :: (PrimMonad m, M.Unbox a) => Bag m a -> m (V.Vector a)
+unsafeFreezeBag :: (PrimMonad m, Unbox a) => Bag m a -> m (V.Vector a)
 unsafeFreezeBag (Bag _ r v) = do
-  m <- readPRef r
-  let v' = M.take m v
+  n <- readPRef r
+  let v' = M.slice 0 n v
   V.unsafeFreeze v'
+
+printBag :: (Unbox a, Show a) => Bag IO a -> IO ()
+printBag = withVectorM print
 
 -- | Thaw a vector with a `Generator` and initial increase factor for
 --   the internal vector. This allows inserting elements.
-thawBag :: (PrimMonad m, Generator g m, M.Unbox a) => g -> Double -> V.Vector a -> m (Bag m a)
+thawBag :: (PrimMonad m, Generator g m, Unbox a) => g -> Double -> V.Vector a -> m (Bag m a)
 thawBag g x v = do
   m <- V.thaw v
   m' <- M.grow m $ ceiling (fromIntegral (V.length v) * x + 10)
@@ -240,3 +340,10 @@ thawBag g x v = do
 -- bagSize :: PrimMonad m => Bag (PrimState m) a -> m Int
 -- bagSize (Bag r _) = readPRef r
 
+-- | Simple for loop.  Counts from /start/ to /end/-1.
+for :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
+for n0 !n f = loop n0
+  where
+    loop i | i == n    = return ()
+           | otherwise = f i >> loop (i+1)
+{-# INLINE for #-}
